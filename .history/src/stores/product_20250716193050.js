9157e0b7e71022e9af611dad710642734f5bc709
manny <eodili@lighthaus-eko.com>
2025-07-16 19:36:46 +0100

    fix: updated base url to fake api

--- .history/src/stores/product_20250716193050.js
+++ .history/src/stores/product_20250716193050.js
@@ -51,8 +51,8 @@ export const useProductStore = defineStore('product', () => {
   async function fetchProductById(id) {
     loading.value = true
     try {
-      const response = await fetch(`${API_BASE_URL}/products/${id}`)
-      // const response = await fetch(`https://fakestoreapi.com/products/${id}`)
+      // const response = await fetch(`${API_BASE_URL}/products/${id}`)
+      const response = await fetch(`https://fakestoreapi.com/products/${id}`)
       const item = await response.json()
       product.value = {
         id: item.id,
